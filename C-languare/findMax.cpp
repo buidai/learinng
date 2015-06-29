be6afb944c5af38ec7724ae6e0a85b932b8f864d
buidai <dainbvn1@gmail.com>
2015-06-29 21:31:17 +0700

    add file findmax.cpp

---
+++ C-languare/findMax.cpp
@@ -0,0 +1,34 @@
+//==========================================================
+/*
+	FileName	: findMax.cpp
+	Author		: bui.quoc.dai
+	Date		: 29/06/2015 (dd/mm/yy)	
+	Note		:
+*/
+//==========================================================
+
+#include "stdio.h"
+#include "conio.h"
+#include "windows.h"
+
+main (){
+	int a,b,max,min;
+    lap:
+	system("cls");
+	printf("\n nhap hai so a,b: ");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+     {
+	   max=a;
+	   min=b;
+     }
+	else
+	  {
+		max=b;
+		min=a;
+      }
+     if(a==b) printf("hai so bang nhau ");
+     else printf("%d la min\n%d la max",min,max);
+	 getch();
+	 goto lap;
+}
